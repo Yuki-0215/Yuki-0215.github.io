623c404d165b2a2220535667ce284aca6fd0e524
Lixie <x.li@openbayes.com>
2025-04-10 18:13:35 +0800

    Auto update: 2025-04-10 18:13:35

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,15 @@
-docker build -t nginx:v1 .
+#!/bin/bash
+
+# Generate a random tag using date and random number
+RANDOM_TAG=$(date +%Y%m%d%H%M%S)-$(openssl rand -hex 4)
+
+# Build the image with the random tag
+docker build -t nginx:${RANDOM_TAG} .
+
+# Tag the image for the target registry
+docker tag nginx:${RANDOM_TAG} uhub.service.ucloud.cn/bayes-common/nginx:${RANDOM_TAG}
+
+# Push the image to the registry
+docker push uhub.service.ucloud.cn/bayes-common/nginx:${RANDOM_TAG}
+
+echo "Image built and pushed with tag: ${RANDOM_TAG}"
