cfcb880431d06229f8eac2c108af591560bdc1e3
Lixie <x.li@openbayes.com>
2025-07-09 15:42:38 +0800

    Auto update: 2025-07-09 15:42:35

--- site/3.develop/python/code/project-ansible/task.py
+++
@@ -1,9 +0,0 @@
-class Task:
-  def __init__(self, name, command):
-    self.name = name
-    self.command = command
-    self.result = None # 任务执行结果
-    self.status = "pending" # 任务状态, pending, success, faild
-  def __repr__(self):
-    """返回任务的字符串表示"""
-    return f"Task({self.name!r}, {self.command!r}, status={self.status!r})"
