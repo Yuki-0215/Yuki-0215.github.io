bdfab2e7e2046485457664efe2db34b7e0a26046
Lixie <x.li@openbayes.com>
2025-07-15 17:35:45 +0800

    Auto update: 2025-07-15 17:35:43

---
+++ site/3.develop/python/code/24-2025-07-15-python-object.py
@@ -0,0 +1,9 @@
+std1 = {'name': 'Tom', 'score': 99}
+std2 = {'name': 'Yuki', 'score': 100}
+
+def print_score(std):
+  print(f"{std['name']}: {std['score']}" )
+
+print_score(std1)
+print_score(std2)
+
---
+++ site/3.develop/python/code/oop/1-oop.py
@@ -0,0 +1,37 @@
+class Student(object):
+  def __init__(self, name, score) -> None:
+    self.name = name
+    self.score = score
+
+  def print_score(self):
+    print(f"{self.name}: {self.score}")
+
+# james = Student('Tom', 99)
+# Yuki = Student('Yuki', 100)
+
+# james.print_score()
+# Yuki.print_score()
+
+class Dog():
+  def __init__(self,name, age):
+    self.name = name
+    self.age = age
+    self.__color = 'white'
+
+  def bark(self):
+    print(f"{self.name} says woof! I'm {self.age} years old")
+
+  def get_color(self):
+    return self.__color
+
+  def set_color(self, color):
+    self.__color = color
+
+# my_dog = Dog('Tom', 3)
+# print(my_dog.__color)
+# my_dog.__color = 'black'
+
+my_dog = Dog('Tom', 3)
+print(my_dog.get_color())
+my_dog.set_color('black')
+print(my_dog.get_color())
